52d0d17861215489514eb9be27ebcf93ebe20d55
Carlos Machado <58347209+cjmach@users.noreply.github.com>
2025-07-27 15:55:46 +0100

    Add AccessTokenType as a top-level enum.

---
+++ jfrdp/src/main/java/pt/cjmach/jfrdp/lib/AccessTokenType.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package pt.cjmach.jfrdp.lib;
+
+/**
+ *
+ * @author mach
+ */
+public enum AccessTokenType {
+    /**
+     * oauth2 access token for RDS AAD authentication
+     */
+    AAD,
+    /**
+     * oauth2 access token for Azure Virtual Desktop
+     */
+    AVD
+}
